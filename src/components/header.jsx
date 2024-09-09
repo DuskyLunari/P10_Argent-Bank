88312174a75037885de0efa5fe4cd05f59e91bfb
Adrien Bolognini <duskylunari@gmail.com>
2024-09-09 16:30:21 +0200

    Fix: image source

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export function Header() {
             <NavLink className="main-nav-logo" to="/">
                 <img
                 className="main-nav-logo-image"
-                src="./img/argentBankLogo.png"
+                src="/argentBankLogo.png"
                 alt="Argent Bank Logo"
                 />
                 <h1 className="sr-only">Argent Bank</h1>
