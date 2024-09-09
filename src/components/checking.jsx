78caf41c0578fdf6b17cbd79cee3ed3e9b771c8a
Adrien Bolognini <duskylunari@gmail.com>
2024-09-09 14:10:40 +0200

    Update: placeholder for components

---
+++ src/components/checking.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Checking() {
+    return (
+        <div>
+            <p>Placeholder Checking</p>
+        </div>
+    );
+}
---
+++ src/components/featureHighlight.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function FeatureHighlight() {
+    return (
+        <div>
+            <p>Placeholder FeatureHighlight</p>
+        </div>
+    );
+}
