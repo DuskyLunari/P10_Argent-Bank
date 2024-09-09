6b3e055e12d35e443c2e85f9c1ebf3e9ccbc0c4f
Adrien Bolognini <duskylunari@gmail.com>
2024-09-09 16:03:14 +0200

    Fix

--- src/components/featureHighlight.jsx
+++ src/components/featureHighlight.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export function FeatureHighlight({ image, altImg, title, text}) {
+export function FeatureHighlight({ image, altimg, title, text}) {
     return (
         <div className="feature-item">
-            <img src={ image } alt={ altImg } className="feature-icon" />
+            <img src={ image } alt={ altimg } className="feature-icon" />
             <h3 className="feature-item-title">{ title }</h3>
             <p>
                 { text }
