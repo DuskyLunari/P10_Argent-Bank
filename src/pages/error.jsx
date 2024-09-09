5d689a7bb2f97c9e9f850363f90f7c9323f5d31c
Adrien Bolognini <duskylunari@gmail.com>
2024-09-09 16:35:20 +0200

    Temporary error page for testing

---
+++ src/pages/error.jsx
@@ -0,0 +1,12 @@
+import { Layout } from "../components/layout";
+
+
+export function Error() {
+    return (
+        <Layout>
+            <main>
+                <p>Error</p>
+            </main>
+        </Layout>
+    )
+}
