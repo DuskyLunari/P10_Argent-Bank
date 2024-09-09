bf50bd332ed6fb2e4531d98b331abc690b160d4e
Adrien Bolognini <duskylunari@gmail.com>
2024-09-09 16:31:54 +0200

    Fix: cleaning component

--- src/components/featureHighlight.jsx
+++ src/components/featureHighlight.jsx
@@ -5,9 +5,7 @@ export function FeatureHighlight({ image, altimg, title, text}) {
         <div className="feature-item">
             <img src={ image } alt={ altimg } className="feature-icon" />
             <h3 className="feature-item-title">{ title }</h3>
-            <p>
-                { text }
-            </p>
+            <p>{ text }</p>
         </div>
     );
 }
