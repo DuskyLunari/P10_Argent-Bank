2eb7ee15cb085472805c6a98666775937e7acd64
Adrien Bolognini <duskylunari@gmail.com>
2024-09-09 16:31:22 +0200

    Update: implemented routing

---
+++ src/router/router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "../pages/Home.jsx";
+import { Login } from "../pages/Login.jsx";
+import { Profile } from "../pages/Profile.jsx";
+import { Error } from "../pages/Error.jsx";
+
+const router = createBrowserRouter( [
+    {
+        path:"/",
+        element: <Home />
+    },
+    {
+        path:"/sign-in",
+        element: <Login />
+    },
+    {
+        path:"/profil",
+        element: <Profile />
+    },
+    {
+        path:"*",
+        element: <Error />
+    },
+])
+
+export default router;
