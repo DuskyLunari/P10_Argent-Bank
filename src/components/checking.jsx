b40ad05d669816cdee22a5f4f2fc36aa1776d555
Adrien Bolognini <duskylunari@gmail.com>
2024-09-11 14:18:50 +0200

    Fix: removed uneeded imports

--- src/components/checking.jsx
+++ src/components/checking.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 export function Checking({ title, balance, description }) {
     return (
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Header } from "./header";
-import { Footer } from "./footer";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 export function Layout({ children }) {
     return (
