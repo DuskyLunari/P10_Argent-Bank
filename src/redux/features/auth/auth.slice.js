eba1a2c8defb1d4d8873b8aa0664da717d1ea7c0
Adrien Bolognini <duskylunari@gmail.com>
2024-09-16 15:42:34 +0200

    Update: Implemented auth slices

---
+++ src/redux/features/auth/auth.slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { logoutAction, loginAction } from "./auth.actions"
+
+const authSlice = createSlice({
+    name: "authSlice",
+    initialState: {
+        token: null,
+        errorMessage: null,
+        isLoading: false,
+    },
+    reducers: {
+        logout: logoutAction
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(loginAction.pending, (state) => {
+            state.isLoading = true;
+            state.errorMessage = null;
+        })
+        .addCase(loginAction.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.token = action.payload;
+        })
+        .addCase(loginAction.rejected, (state, action) => {
+            state.isLoading = false,
+            state.errorMessage = action.payload;
+        });
+    }
+});
+
+export { authSlice };
