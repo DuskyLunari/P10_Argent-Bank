c068b3ad74c82ee017ed01afee7205b244d51cff
Adrien Bolognini <duskylunari@gmail.com>
2024-09-16 22:07:25 +0200

    Update: user name is injected from API call

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,14 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Layout } from "../components/Layout.jsx";
 import { Checking } from "../components/Checking.jsx";
+import { getUser } from "../redux/features/profile/profile.actions.js";
 
 export function Profile() {
+  const dispatch = useDispatch();
+
+  const { firstName, lastName, isLoading, errorMessage } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (errorMessage) {
+    return <p>Error: {errorMessage}</p>;
+  }
+
   return (
     <Layout>
-     <main className="main bg-dark">
-      <div className="header">
-        <h1>Welcome back<br />Tony Jarvis!</h1>
-        <button className="edit-button">Edit Name</button>
-      </div>
+      <main className="main bg-dark">
+        <div className="header">
+          <h1>
+            Welcome back<br />
+            {firstName} {lastName}!
+          </h1>
+          <button className="edit-button">Edit Name</button>
+        </div>
 
         <h2 className="sr-only">Accounts</h2>
         <Checking
