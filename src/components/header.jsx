5c700603f3abfb31c65bfb7139ca93e98089e477
Adrien Bolognini <duskylunari@gmail.com>
2024-09-16 18:17:43 +0200

    Update: using Redux to update header to different states

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,57 @@
-import { NavLink } from "react-router-dom";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { logout } from "../redux/features/auth/auth.slice";
 
 export function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
   return (
     <header>
-        <nav className="main-nav">
-            <NavLink className="main-nav-logo" to="/">
-                <img
-                className="main-nav-logo-image"
-                src="/argentBankLogo.png"
-                alt="Argent Bank Logo"
-                />
-                <h1 className="sr-only">Argent Bank</h1>
-            </NavLink>
-            <div>
-                <NavLink className="main-nav-item" to="/sign-in">
+      <nav className="main-nav">
+        <NavLink className="main-nav-logo" to="/">
+          <img
+            className="main-nav-logo-image"
+            src="/argentBankLogo.png"
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </NavLink>
+        <div>
+          {token && (
+            <>
+              <NavLink className="main-nav-item" to="/profile">
                 <i className="fa fa-user-circle"></i>
-                Sign In
-                </NavLink>
-            </div>
-        </nav>
+                Tony
+              </NavLink>
+              <NavLink 
+                className="main-nav-item"
+                to="/"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleLogout();
+                }}
+              >
+                <i className="fa fa-sign-out"></i>
+                Logout
+              </NavLink>
+            </>
+          )}
+          {!token && (
+            <NavLink className="main-nav-item" to="/sign-in">
+              <i className="fa fa-user-circle"></i>
+              Sign In
+            </NavLink>
+          )}
+        </div>
+      </nav>
     </header>
   );
 }
