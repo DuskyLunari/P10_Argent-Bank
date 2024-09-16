f53af9d0adc621a2a223bd2140c98a9f67e12eea
Adrien Bolognini <duskylunari@gmail.com>
2024-09-16 21:09:36 +0200

    Fix: import typo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Layout } from "../components/layout";
+import { Layout } from "../components/Layout";
 import { FeatureHighlight } from "../components/featureHighlight";
 
 export function Home() {
