b1dbb36d590a59eb5211622c3d3e90f585abf569
Adrien Bolognini <duskylunari@gmail.com>
2024-09-18 16:40:54 +0200

    Fix: update auth umport & reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authSlice } from "./features/auth/auth.slice"; 
+import authReducer from "./features/auth/auth.slice"; 
 import profileReducer from "./features/profile/profile.slice";
 
 export const store = configureStore({
     reducer: {
-        auth: authSlice.reducer,
+        auth: authReducer,
         user: profileReducer,
     }
 });
